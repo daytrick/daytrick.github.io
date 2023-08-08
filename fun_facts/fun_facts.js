a0717ad5cbcb8e7cb5b1a6723ff6030816660e01
daytrick <120808108+daytrick@users.noreply.github.com>
2023-08-08 15:58:25 +0800

    Trying to query a random fact

--- fun_facts/fun_facts.js
+++ fun_facts/fun_facts.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 // Import method from: https://stackoverflow.com/a/69491450
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+import { getFirestore, AutoId } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { collection, doc, getDoc, getDocs, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 
@@ -22,8 +22,58 @@ const app = initializeApp(config);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const facts = db.collection("funfacts");
 
-// Get a document
+
+/**
+ * Query for a random fact from the database.
+ * 
+ * How to query a random doc from: https://stackoverflow.com/a/46801925
+ */
+function getRandomFact() {
+
+    // Generate a random ID
+    let randID = new AutoId.newId();
+    console.log(randID);
+
+    // Get a random fact
+    // How to use where from: https://firebase.google.com/docs/firestore/query-data/queries?hl=en&authuser=0
+    // How to order and limit from: https://firebase.google.com/docs/firestore/query-data/order-limit-data?authuser=0&hl=en
+    let query = facts.where("__name__", ">=", randID)
+                     .orderBy("__name__")
+                     .limit(1);
+
+    query.get().then((querySnapshot) => {
+
+        // Get first fact
+        if (querySnapshot.empty) {
+
+            let backupQuery = facts.where("__name__", "<", randID)
+                                   .orderBy("__name__")
+                                   .limit(1);
+            
+            // Will succeed by now, as long as there's at least one doc in the collection
+            backupQuery.get().then((backupQuerySnapshot) => {
+                showFact(backupQuerySnapshot);
+            });
+            
+        }
+        else {
+            showFact(querySnapshot);
+        }
+    });
+
+}
+
+
+
+function showFact(querySnapshot) {
+
+    console.log(querySnapshot);
+
+}
+
+/*// Get a document
 const docRef = doc(db, "funfacts");
 const docSnap = await getDoc(docRef);
 
@@ -32,4 +82,4 @@ if (docSnap.exists()) {
 } else {
   // docSnap.data() will be undefined in this case
   console.log("No such document!");
-}
+}*/
