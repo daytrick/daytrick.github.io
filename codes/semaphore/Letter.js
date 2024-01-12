5b90ba02595e51c399e6fa98501b32b490742ba2
daytrick <120808108+daytrick@users.noreply.github.com>
2024-01-12 18:43:07 +0000

    Undoing merge muckery (#68)

--- codes/semaphore/Letter.js
+++ codes/semaphore/Letter.js
@@ -102,13 +102,6 @@ class Letter {
     
                 if (this.#checkStrokeIntersection(stroke1, stroke2, letter1.endpoint)) {
                     console.log("Found intersection: " + letter1.letter + " and " + letter2.letter);
-            return false;
-        }
-    
-        for (const stroke1 of letter1.strokes()) {
-            for (const stroke2 of letter2.strokes()) {
-    
-                if (this.#checkStrokeIntersection(stroke1, stroke2)) {
                     return true;
                 }
     
@@ -273,7 +266,6 @@ class Letter {
             return (intersect && somewhereOtherThanTheJoin);
         }
         // If get to here, they do not cross
-        return false;
     
     }
 
@@ -288,4 +280,5 @@ class UnencodableError extends Error {
         this.message = "Unencodable character found: " + char;
         this.name = "UnencodableError";
     }
+
 }
