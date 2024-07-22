03108562f12cdbc42923f2086293753afd5542da
daytrick <120808108+daytrick@users.noreply.github.com>
2024-07-22 11:16:51 +0100

    Got desired text/pfp layout

---
+++ about/about.js
@@ -0,0 +1,51 @@
+const MAX_BLINK_INTERVAL = 20 * 1000;
+
+const imgDiv = document.getElementById("aboutImage");
+var pfpOpen = null;
+var pfpBlink = null;
+
+function load() {
+
+    pfpOpen = loadImage("./pfps/pfp_open.png", true);
+    pfpBlink = loadImage("./pfps/pfp_blink.png", false);
+    blink();
+
+}
+
+function loadImage(path, visible) {
+
+    let img = document.createElement("img");
+    img.src = path;
+    img.classList.add("pfp");
+    // img.style.setProperty("object-fit", "contain");
+    // img.style.setProperty("max-width", "100%");
+    // img.style.setProperty("max-height", "100%");
+    if (!visible) {
+        img.hidden = true;
+    }
+    imgDiv.appendChild(img);
+
+    return img;
+
+}
+
+function blink() {
+
+    let timeout = Math.random() * MAX_BLINK_INTERVAL;
+    setTimeout(() => {
+        
+        pfpOpen.hidden = true;
+        pfpBlink.removeAttribute("hidden");
+
+        setTimeout(() => {
+
+            pfpBlink.hidden = true;
+            pfpOpen.removeAttribute("hidden");
+
+        }, 200);
+        
+        blink();
+
+    }, timeout);
+
+}
